10f5fc0948908c50f21907f3b9fc400dee013bfd
VictorPettenon <pettenon.r3cotia@gmail.com>
2013-02-20 10:52:38 -0300

    Blog Game Engines

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  attr_accessible :content, :name, :tag_list
+
+  acts_as_taggable
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '64253ce4f6b8b807e37af4392857395397ddaee1ec99c78256cbd1f6e0aff0247702e8775cc2b7cdba6ef69c439415286d9f9241c4e6b186d96ac29fac4857d6'
